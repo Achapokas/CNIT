5227dcc9c640e2869d20b3bedf6b3775e01d79b2
Ashley Chapokas <achapokas@weblinc.com>
2018-01-24 16:39:52 -0800

    CNIT: Completes first JS problem set

---
+++ achapokas/assets/javascripts/hw/hw2/part_1.js
@@ -0,0 +1,4 @@
+var letters = "<span style='color: red; font-family: \"TimesNewRoman\", Times, serif;'>ab </span>",
+    numBlue = "<span style='color: blue; font-family: Arial, sans-serif'>12 </span>"
+    numGreen = "<span style='color: green; font-family: impact; font-style: italic;'>34 </span>"
+document.write("<strong>" + letters + '' + numBlue + '' + numGreen + "</strong>");
